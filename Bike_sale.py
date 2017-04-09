e2c56303bb5f212f20fa19a304e4756392f33baa
crystalattice <crystalattice@gmail.com>
2017-04-08 19:24:23 -0500

    Rewrote code
    Changed inventory to dictionary from list and adjusted all subsequent calls to work with Bicycle objects instead of list values

--- Bike_sale.py
+++ Bike_sale.py
@@ -1,6 +1,8 @@
-#TODO: change inventory to dictionary and adjust all related data structures
+from collections import defaultdict
 
-class Bicycle():
+
+class Bicycle:
+    """Models a bicycle with model, weight, and cost."""
     def __init__(self, model, weight, cost):
         self.model = model
         self.weight = weight
@@ -11,68 +13,96 @@ class Bicycle():
 
         Includes the name of the class (Bicycle), the model, the weight, and the cost
         """
-        return "%s(model=%r, weight=%r, cost=%r)" % (self.__class__.__name__, self.model, self.weight, self.cost)
+        return "{model}, {weight}, {cost}".format(model=self.model, weight=self.weight, cost=self.cost)
 
 
-class Shop():
+class Shop:
+    """Models a bike shop.
+    
+    Creates and shows the bike inventory, calculates profit, and removes bikes from inventory when sold.
+    """
     def __init__(self, name):
         self.name = name
-        self.inventory = [] #Change to dictionary
+        self.inventory = {}
+        self.avail_inventory = []
         self.markup = 0.2
         self.store_profit = 0
+        self.sel_bike_price = None
 
+    def initial_inventory(self, bicycle, quantity):
+        """Creates initial inventory of shop inventory"""
+        self.inventory[bicycle] = quantity
 
-    def set_inventory(self, bike_name):
-        self.inventory.append(bike_name)
+    def update_inventory(self, bicycle):
+        """Allows adding new bikes to inventory (Not implemented)"""
+        self.inventory[bicycle] += 1
 
     def show_inventory(self):
-        return self.inventory
+        """Displays bikes currently available"""
+        for bicycle in self.inventory:
+            if self.inventory[bicycle] > 0:
+                self.avail_inventory.append(bicycle)
+        return self.avail_inventory
 
-    def calc_profit(self, selected_bike):
-        self.sel_bike = self.inventory[selected_bike]
-        #print (self.sel_bike.cost)
-        self.store_profit += self.sel_bike.cost * self.markup
-        return self.store_profit
+    def calc_profit(self, bike_cost):
+        """Calculate how much profit the store made on a sale"""
+        self.sel_bike_price = bike_cost
+        self.store_profit += self.sel_bike_price * self.markup
 
-    def sell_bike(self, choice):
-        self.selection = self.inventory.pop(choice)
-        return self.selection
+    def sell_bike(self, bike_name):
+        """Remove a sold bike from inventory"""
+        self.inventory[bike_name] -= 1
 
-class Customer():
+
+class Customer:
+    """Models a customer.
+    
+    Gives a customer name and spending money. Determines which bikes are available based on available money. Provides
+    purchased bike to Shop class for inventory management."""
     def __init__(self, name, money):
         self.name = name
         self.money = money
-        self.bike_list = []
+        self.bike_selection = []
+        self.customer_avail = []
+        self.bike_choice = None
+        self.bike_sold = None
 
-    def bikes_avail(self, bike):
-        if bike.cost <= self.money:
-            self.bike_list.append(bike.model)
+    def bikes_avail(self, bicycle):
+        """Determines list of bikes within customer price range"""
+        if bicycle.cost <= self.money:
+            self.bike_selection.append(bicycle)
 
     def show_bikes_avail(self):
-        return self.bike_list
+        """Prints list of bikes in customer's price range"""
+        print("These are the bicycles available in your price range:")
+        for bicycle in self.bike_selection:
+            self.customer_avail.append(bicycle.model)
+        return self.customer_avail
 
     def purchase(self):
+        """Selects a bike for purchase, removes bike from shop inventory, calculates shop's profit, and calculates
+        how much money customer has left"""
         self.bike_choice = input("Which would you like to purchase? ")
         if self.bike_choice == "Corvair":
-            self.selected_bike = 0
+            self.bike_sold = bike1
         elif self.bike_choice == "Airstream":
-            self.selected_bike = 1
+            self.bike_sold = bike2
         elif self.bike_choice == "Slipstream":
-            self.selected_bike = 2
+            self.bike_sold = bike3
         elif self.bike_choice == "Monty":
-            self.selected_bike = 3
+            self.bike_sold = bike4
         elif self.bike_choice == "Zuul":
-            self.selected_bike = 4
+            self.bike_sold = bike5
         elif self.bike_choice == "Ratman":
-            self.selected_bike = 5
+            self.bike_sold = bike6
         else:
-            print ("You gave an invalid selection")
-            
-        shop.calc_profit(self.selected_bike)
+            print("You gave an invalid selection")
 
-        self.sold_bike = shop.sell_bike(self.selected_bike)
-        print(self.sold_bike)
-        print("Money left in pocket: $" + str(self.money - self.sold_bike.cost))
+        shop.calc_profit(self.bike_sold.cost)
+        shop.sell_bike(self.bike_sold)
+
+        print("You purchased {}".format(self.bike_sold.model))
+        print("Money left in pocket: $" + str(self.money - self.bike_sold.cost))
 
 
 if __name__ == "__main__":
@@ -82,6 +112,7 @@ if __name__ == "__main__":
 
     shop = Shop("Blannigan's Bikes")
 
+    # Create each bike object
     bike1 = Bicycle("Corvair", 20, 150)
     bike2 = Bicycle("Airstream", 18, 200)
     bike3 = Bicycle("Slipstream", 25, 100)
@@ -89,36 +120,43 @@ if __name__ == "__main__":
     bike5 = Bicycle("Zuul", 10, 800)
     bike6 = Bicycle("Ratman", 13, 650)
 
-    shop.set_inventory(bike1)
-    shop.set_inventory(bike2)
-    shop.set_inventory(bike3)
-    shop.set_inventory(bike4)
-    shop.set_inventory(bike5)
-    shop.set_inventory(bike6)
+    shop.initial_inventory(bike1, 1)
+    shop.initial_inventory(bike2, 1)
+    shop.initial_inventory(bike3, 1)
+    shop.initial_inventory(bike4, 1)
+    shop.initial_inventory(bike5, 1)
+    shop.initial_inventory(bike6, 1)
 
-    print(shop.name)
-    print(shop.show_inventory())
+    print("Welcome to {}".format(shop.name))
+    print("Here the bikes we carry:")
+    for bicycle in shop.inventory:
+        print("Name: {}".format(bicycle.model))
+        print("Weight: {} lbs".format(bicycle.weight))
+        print("Cost: ${}".format(bicycle.cost))
 
-    print("Dan's budget = $%f" % dan.money)
+    print("Dan's budget = ${}".format(dan.money))
     for bike in shop.show_inventory():
         dan.bikes_avail(bike)
-    print(dan.show_bikes_avail())
+    for item in dan.show_bikes_avail():
+        print(item)
     dan.purchase()
-    print(shop.show_inventory())
-    print(shop.store_profit)
+    print(shop.inventory)
+    print("Store profit: ${}".format(shop.store_profit))
 
-    print("Jessie's budget = $%f" % jessie.money)
+    print("Jessie's budget = {}".format(jessie.money))
     for bike in shop.show_inventory():
         jessie.bikes_avail(bike)
-    print(jessie.show_bikes_avail())
+    for item in jessie.show_bikes_avail():
+        print(item)
     jessie.purchase()
-    print(shop.show_inventory())
-    print(shop.store_profit)
-    
-    print("Brock's budget = $%f" % brock.money)
+    print(shop.inventory)
+    print("Store profit: ${}".format(shop.store_profit))
+
+    print("Brocks's budget = {}".format(brock.money))
     for bike in shop.show_inventory():
         brock.bikes_avail(bike)
-    print(brock.show_bikes_avail())
+    for item in brock.show_bikes_avail():
+        print(item)
     brock.purchase()
-    print(shop.show_inventory())
-    print(shop.store_profit)
+    print(shop.inventory)
+    print("Store profit: ${}".format(shop.store_profit))
